9818d9e52f567b04c419905677a22a63f620c955
Mohamed jinas <j@live.mv>
2020-03-28 16:41:14 +0500

    added globalstats tests

---
+++ tests/GlobalStatsTest.php
@@ -0,0 +1,66 @@
+<?php
+
+class GlobalStatsTest extends \PHPUnit\Framework\TestCase
+{
+    public function testIfGetTotalIsValid()
+    {
+        $globalstats = new \Jinas\Covid19\GlobalStats;
+        $array = $globalstats->GetTotal();
+
+        $this->assertIsArray($array);
+
+        $this->assertArrayHasKey('total_confirmed', $array);
+        $this->assertArrayHasKey('total_recovered', $array);
+        $this->assertArrayHasKey('total_deaths', $array);
+    }
+
+
+    public function testIfGetAllIsValid()
+    {
+        $globalstats = new \Jinas\Covid19\GlobalStats;
+        $response = $globalstats->GetAll();
+        $array = $response[0];
+
+        $this->assertIsArray($globalstats->GetAll());
+
+        $this->assertArrayHasKey('Province_State', $array);
+        $this->assertArrayHasKey('Country_Region', $array);
+        $this->assertArrayHasKey('Confirmed', $array);
+        $this->assertArrayHasKey('Recovered', $array);
+        $this->assertArrayHasKey('Deaths', $array);
+        $this->assertArrayHasKey('Active', $array);
+        $this->assertArrayHasKey('Lat', $array);
+        $this->assertArrayHasKey('Long_', $array);
+    }
+
+    public function testIfGetAllCountriesReturnsAnArray()
+    {
+        $globalstats = new \Jinas\Covid19\GlobalStats;
+        $this->assertIsArray($globalstats->GetAllCountries());
+    }
+
+    public function testIfGetTotalByCountryIsValid()
+    {
+        $globalstats = new \Jinas\Covid19\GlobalStats;
+
+        $response = $globalstats->GetTotalByCountry();
+        $array = $response[0];
+
+        $this->assertIsArray($globalstats->GetTotalByCountry());
+        
+        $this->assertArrayHasKey('country', $array);
+        $this->assertArrayHasKey('confirmed', $array);
+        $this->assertArrayHasKey('recovered', $array);
+        $this->assertArrayHasKey('deaths', $array);
+    }
+
+    public function testIfGetAllGroupedByCountryIsValid()
+    {
+        $globalstats = new \Jinas\Covid19\GlobalStats;
+
+        $countries = $globalstats->GetAllCountries();
+        $array = $globalstats->GetAllGroupedByCountry();
+
+        $this->assertArrayHasKey($countries[0], $array);
+    }
+}
