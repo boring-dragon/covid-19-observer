316619e8c3d9a48703d24779c67e46872387f69c
Mohamed Jinas <j@live.mv>
2020-05-07 11:15:14 +0500

    Maldives Cases added
    Added mv classes to filter through cases in maldives.

---
+++ src/MV/MvCases.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Jinas\Covid19\MV;
+
+use Exception;
+use Jinas\Covid19\Http\Client;
+use Tightenco\Collect\Support\Arr;
+
+class MvCases
+{
+    protected const API_URL = "https://covid19.health.gov.mv/api/fetch_cases";
+
+    protected $api_response;
+
+
+    public function __construct()
+    {
+        $this->FetchCases();
+    }
+    
+    /**
+     * GetAll
+     * 
+     *  Get all returned by the API
+     *
+     * @return array
+     */
+    public function GetAll() : array
+    {
+        return $this->api_response;
+    }
+    
+    /**
+     * GetCases
+     * 
+     *  Get all the cases returned by the API
+     *
+     * @return array
+     */
+    public function GetCases() : array
+    {
+        return $this->api_response["cases"];
+    }
+    
+    /**
+     * FilterCasesByAge
+     * 
+     *  Filter cases by there age
+     *
+     * @param  string $operator
+     * @param  mixed $value
+     * @return array
+     */
+    public function FilterCasesByAge(string $operator, $value) : array
+    {
+        $cases = collect($this->GetCases());
+
+        $filtered = $cases->where('age', $operator, $value);
+
+        return $filtered->toArray();
+    }
+    
+    /**
+     * FilterCasesByNationality
+     * 
+     *  Filter cases by nationality.
+     * 
+     * Ex: FilterCasesByNationality("Bangladesh")
+     *
+     * @param  mixed $nationality
+     * @return void
+     */
+    public function FilterCasesByNationality(string $nationality) : array
+    {
+        $cases = collect($this->GetCases());
+
+        $filtered = $cases->where('nationality', '=', $nationality);
+
+        return $filtered->toArray();
+
+    }
+    
+    /**
+     * FilterCasesByGender
+     * 
+     *  Filter cases by gender
+     * 
+     *  Ex: FilterCasesByGender("female");
+     *
+     * @param  mixed $gender
+     * @return void
+     */
+    public function FilterCasesByGender(string $gender) : array
+    {        
+        $cases = collect($this->GetCases());
+
+        $filtered = $cases->where('gender', '=', ucfirst($gender));
+
+        return $filtered->toArray();
+
+    }
+
+     /**
+     * FetchCases
+     *
+     *  Fetch the cases from API
+     *
+     * @return void
+     */
+    protected function FetchCases(): void
+    {
+        $client = new Client;
+        $this->api_response = $client->get($this::API_URL);
+    }
+}
